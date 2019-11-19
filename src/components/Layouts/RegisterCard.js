6cbefaa2a86d7d749bb578baea172301b2ed05de
adripleitez <00067218@uca.edu.sv>
2019-11-19 09:38:05 -0600

    Adding accounts views

---
+++ src/components/Layouts/RegisterCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+class RegisterCard extends React.Component {
+	render() {
+		let account = this.props.account;
+		return (
+			<Card style={{ minWidth: '400px', backgroundColor: '#7cffcb', 
+                margin: '2%', backgroundImage:'linear-gradient(315deg, #7cffcb 0%, #74f2ce 74%)'}}>
+				<CardActionArea>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="h2">
+							{account.name}
+						</Typography>
+						<Typography variant="body1" color="textSecondary" component="p">
+							Tipo de cuenta: {account.type}
+						</Typography>
+						<Typography variant="body1" color="textSecondary" component="p">
+							Monto: ${account.amount}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+				<CardActions>
+					<Button
+						size="small"
+						color="primary"
+						onClick={() => {
+							this.props.onDelete();
+						}}
+					> Eliminar
+					</Button>
+				</CardActions>
+			</Card>
+		);
+	}
+}
+
+export default RegisterCard;
--- src/components/Layouts/viewAcounts.js
+++ src/components/Layouts/viewAcounts.js
@@ -1,115 +1,125 @@
 import React from 'react';
 import API from '../../utils/apiUrlBase';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import AddAccount from "./AddAccount";
-
-
-const useStyles = makeStyles(theme => ({
-  card: {
-    maxWidth: 345
-  },
-  media: {
-    height: 140
-  }
-}));
-
-
-function MediaCard(props) {
-  const classes = useStyles();
-  return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.accounts.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across
-            all continents except Antarctica
-						</Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-					</Button>
-        <Button size="small" color="primary">
-          Learn More
-					</Button>
-      </CardActions>
-    </Card>
-  );
-}
+import AddAccount from './AddAccount';
+import RegisterCard from './RegisterCard';
 
 export default class Views extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			account_list: []
+		};
+	}
+
+	// componentDidMount() {
+	//   let options = {
+	//     headers: {
+	//       Accept: 'application/json'
+	//     }
+	//   };
+	//   fetch(`${API.baseURL}/accounts`, options)
+	//     .then((res) => {
+	//       return res.json();
+	//     })
+	//     .then((data) => {
+	//       console.log(data);
+	//       data.map((element) => this.state.account_list.push(element));
+	//     })
+	//     .then(console.log(this.state.account_list))
+	//     .catch((err) => console.log('Ocurrio un error en la conexion'));
+	// }
+
+	componentDidMount() {
+		let options = {
+			headers: {
+				Accept: 'application/json'
+			}
+		};
+		fetch(`${API.baseURL}/accounts`, options)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				console.log(data);
+				data.map((element) => this.state.account_list.push(element));
+			})
+			.catch((err) => console.log('Ocurrio un error en la conexion'));
+	}
+
+	handleSubmit(account) {
+		let options = {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+				Accept: 'application/json'
+			},
+
+			body: JSON.stringify(account)
+		};
+
+		fetch(`${API.baseURL}/accounts/create`, options)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				console.log(data);
+				let list = this.state.account_list.slice();
+
+				this.setState({
+					account_list: list.concat([ data.account ])
+				});
+			})
+			.catch((err) => console.log('Ocurrio un error en la conexion'));
+  }
   
-  constructor(props) {
-    super(props);
-    this.state = {
-      account_list: []
-    };
-  }
-
-  componentDidMount() {
-    let options = {
-      headers: {
-        Accept: 'application/json'
-      }
-    };
-    fetch(`${API.baseURL}/accounts`, options)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        data.map((element) => this.state.account_list.push(element));
-      })
-      .then(console.log(this.state.account_list))
-      .catch((err) => console.log('Ocurrio un error en la conexion'));
-  }
-
-  handleSubmit(account){
+  handleDelete(account){
 
     let options ={
-        method : "POST",
+        method : "DELETE",
         headers : {
             "Content-type" : "application/json",
             Accept: "application/json"
         },
 
-        body :  JSON.stringify(account)
+        body :  JSON.stringify({id:account._id})
     }
 
-    fetch(`${API.baseURL}/accounts/create`,options)
+    fetch(`${API.baseURL}/accounts/delete`,options)
     .then(res =>{return res.json()})
     .then(data=>{
         console.log(data);
-        let list = this.state.account_list.slice();
-      
+
+        let index = this.state.account_list.find(value=>{
+            return value._id === account._id;
+        })
+
+        let buffer_list = this.state.account_list.slice();
+        buffer_list.splice(index, 1);
+
         this.setState({
-            account_list: list.concat([data.account]),
+            account_list: buffer_list
         });
     })
-    .catch(err => console.log("Ocurrio un error en la conexion")) 
-  }
+    .catch(err => console.log("Ocurrio un error en la conexion"))
+}
 
-  render() {
-    return (
-      <div style={{ width: '100%'}}>
-        {this.state.account_list.length === 0 ? (
-          <AddAccount onSubmit = {(account)=>{this.handleSubmit(account);}}/>
-        ) : (
-          this.state.account_list.map((w, i) => {
-              {/* return <MediaCard nameAccount={this.state.account_list[i]} /> */}
-            })
-          )}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div style={{ width: '100%' }}>
+				<AddAccount
+					onSubmit={(account) => {
+						this.handleSubmit(account);
+					}}
+				/>
+
+        <div style={{display:'flex', flexFlow: 'row wrap', justifyContent: 'center'}}>
+          {this.state.account_list.map((element) => {
+            return <RegisterCard key={element._id} account={element} onDelete= {()=>{
+                    this.handleDelete(element);
+                }} />;
+          })}
+        </div>
+			</div>
+		);
+	}
 }
